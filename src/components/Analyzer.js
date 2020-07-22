dabf383adba3171188fdf54673c902d5b6a97b79
Anna <anna.sidorova.feri@gmail.com>
2020-07-22 13:37:21 +0300

    Added splitting to separate words

--- src/components/Analyzer.js
+++ src/components/Analyzer.js
@@ -5,22 +5,26 @@ import WordsOutput from './WordsOutput';
 class Analyzer extends React.Component {
     state = {
         words: [],
+        known_words: [ "apples"],
         unknown_words: []
     }
 
     process = (text) => {
         console.log(text)
-        let array = text.split(' ')
-        console.log(array)
-        this.setState({ words: array})
+        let array = text.replace(/[^a-zA-Z' ]+/g, '').replace(/\s\s+/g, ' ').split(' ')
+        this.setState({ words: array })
+        let unkn = array.filter(w => {
+            return this.state.known_words.find(e => { return e === w }) === undefined
+        })
+        this.setState({ unknown_words: unkn })
     }
 
-    render(){
+    render() {
         return (
         <div>
             <p>Hello from analyzer</p>
             <TextInput process={ this.process } />
-            <WordsOutput words={ this.state.words } />
+            <WordsOutput words={ this.state.unknown_words } />
         </div>
         );
     }
