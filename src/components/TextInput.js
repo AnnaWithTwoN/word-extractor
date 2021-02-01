ddb080c8c6a9e1180da00511237acbce29224602
Anna <anna.sidorova.feri@gmail.com>
2021-02-01 22:22:35 +0100

    Added file upload support

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -2,11 +2,25 @@ import React from 'react';
 
 class TextInput extends React.Component {
     state = {
-        text : ''
+        text : '',
+        file: ''
     }
 
     onChange = (e) => {
+        console.log(e)
         this.setState({ [e.target.name]: e.target.value })
+    }
+
+    onFileChosen = (e) => {
+        let file = e.target.files[0]
+
+        const reader = new FileReader();
+
+        reader.addEventListener('load', (event) => {
+            console.log(event.target.result);
+            this.setState({ text: event.target.result })
+        });
+        reader.readAsText(file);
     }
 
     onSubmit = (e) =>{
@@ -16,18 +30,19 @@ class TextInput extends React.Component {
 
     render(){
         return (
-            
         <form onSubmit={ this.onSubmit }>
-            <textarea className="form-control mb-3" rows="5" name="text" value={ this.state.text } onChange={ this.onChange }></textarea>
+            <textarea className="form-control mb-3" rows="5" name="text" 
+                value={ this.state.text } onChange={ this.onChange }></textarea>
             <div className="form-group">
                 <label className="mr-3">Or select a file:</label>
-                <input type="file" name="subtitles_file"/>
+                <input type="file" name="subtitles_file" 
+                    value={ this.state.file } onChange={ this.onFileChosen }
+                    accept=".srt, .ssa, .ttml, .sbv, .dfxp, .vtt, .txt"/>
             </div>
             <input className="btn btn-primary" type="submit" name="submit" />
         </form>
         );
     }
-  
 }
 
 export default TextInput;
